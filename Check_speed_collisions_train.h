89ac86ae2b3d24c7ffaae24638b7b6a055be63a0
MrSteelRat <drebin341@gmail.com>
2024-11-27 18:31:40 +0300

    Add project files.

---
+++ Check_speed_collisions_train.h
@@ -0,0 +1,8 @@
+﻿// Check_speed_collisions_train_cmake.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
